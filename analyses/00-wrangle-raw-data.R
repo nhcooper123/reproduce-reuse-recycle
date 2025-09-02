eb21b6a12b4009f96190b07fb6358d1cf6df4c17
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2025-09-02 13:33:14 +0100

    Update with new data

--- analyses/00-wrangle-raw-data.R
+++ analyses/00-wrangle-raw-data.R
@@ -1,12 +1,12 @@
 # Wrangle data from BES system
-# July 2025
+# Sept 2025
 
 # Load libraries
 library(tidyverse) # for data manipulation and plotting
 library(janitor) # for tidying names
 
 # Read in the dataset
-raw_data <- read_csv("raw-data/BES article metadata 2015-2024.csv")
+raw_data <- read_csv("raw-data/BES_2015-2024_article_data_2025-09-02.csv")
 # Check it read in properly
 glimpse(raw_data)
 
@@ -17,35 +17,42 @@ raw_data <- clean_names(raw_data)
 # Clean the data
 clean_data <- 
   raw_data %>%
+# Rename article 
+  mutate(category_display_article_type = 
+          case_when(category_display_article_type == "Long-term Studies in Animal Ecology" ~ "Long Term Study",
+                    category_display_article_type == "Original Article" ~ "Research Article",
+                    category_display_article_type == "Special Issue Article" ~ "Research Article",
+                    category_display_article_type == "Standard Paper" ~ "Research Article",                               
+                    TRUE ~ as.character(category_display_article_type))) %>%
 # Select only required article types
-  filter(display_article_type == "Research Article" |
-          display_article_type == "Original Research" | 
-          display_article_type == "Standard Paper" | 
-          display_article_type == "Original Article" |
-          display_article_type == "Original Articles" | 
-          display_article_type == "Application" |
-          display_article_type == "Practical Tools") %>%
-# Remove miscallenous errors in article types
-  filter(base_article_type != "Editorial" &
-          base_article_type != "Guest Editorial" &
-          base_article_type != "Commentary") %>%
-# Combine identical article types
-  mutate(display_article_type = str_replace_all(display_article_type, "Original Articles", "Research Article")) %>%                           
-  mutate(display_article_type = str_replace_all(display_article_type, "Original Article", "Research Article")) %>%  
-  mutate(display_article_type = str_replace_all(display_article_type, "Standard Paper", "Research Article")) %>%  
-  mutate(display_article_type = str_replace_all(display_article_type, "Original Research", "Research Article")) %>%
+  # i.e. Research Article, Applications, Practical Tools, Data Articles and Long Term Study
+
+  # Excludes Biological Flora, Commentary, Concept, Correction,
+  # Correspondence, Corrigendum, Editorial, Editorial Note, Erratum,
+  # Essay Review, FE Spotlight, Forum, From Practice,
+  # Guest Editorial, How To, In Focus, Mini review, Perspectives,
+  # Policy Directions, Practice Insights, Practitioner Perspective,
+  # Registered Reports, Research Highlights, Research Methods Guides,
+  # Review, Review and Synthesis, Synthesis.
+  
+  filter(category_display_article_type == "Research Article" |
+           category_display_article_type == "Application" | 
+           category_display_article_type == "Data Article" | 
+           category_display_article_type == "Long Term Study" |
+           category_display_article_type == "Practical Tools") %>%
 # Write journal names in full
-  mutate(journal = str_replace_all(journal, "FEC", "Functional Ecology")) %>%                           
-  mutate(journal = str_replace_all(journal, "MEE3", "Methods in Ecology and Evolution")) %>%  
-  mutate(journal = str_replace_all(journal, "PAN3", "People and Nature")) %>%  
-  mutate(journal = str_replace_all(journal, "JANE", "Journal of Animal Ecology")) %>%
-  mutate(journal = str_replace_all(journal, "ECE3", "Ecology and Evolution")) %>% 
-  #mutate(journal = str_replace_all(journal, "JE", "Journal of Ecology")) %>%  
-  mutate(journal = str_replace_all(journal, "JPE", "Journal of Applied Ecology")) %>%
-  mutate(journal = str_replace_all(journal, "ESO3", "Ecological Solutions and Evidence")) %>%
+  mutate(journal = 
+           case_when(journal == "FEC" ~ "Functional Ecology",
+                     journal == "MEE3" ~ "Methods in Ecology and Evolution",
+                     journal == "PAN3" ~ "People and Nature",
+                     journal == "JANE" ~ "Journal of Animal Ecology",
+                     journal == "JEC" ~ "Journal of Ecology", 
+                     journal == "JPE" ~ "Journal of Applied Ecology",
+                     journal == "ESO3" ~ "Ecological Solutions and Evidence",
+                     TRUE ~ as.character(journal))) %>%
 # Select only the variables we need
-dplyr::select(doi_link, doi, journal, article_type = display_article_type, article_title, 
-              date_published = final_vo_r, corresponding_author_country)
+dplyr::select(doi_link, doi, journal, article_type = category_display_article_type, article_title, 
+              date_published = first_online_actual, corresponding_author_country)
 
 # Randomly order the papers
 clean_data_random <- clean_data[sample(nrow(clean_data)), ]
@@ -71,8 +78,9 @@ ggplot(clean_data_random, aes(x = journal, fill = article_type)) +
   geom_bar() +
   coord_flip() +
   ylab("Number of papers") +
-  scale_fill_manual(values = c("red","yellow","grey"))
+  scale_fill_manual(values = c("red","yellow","grey", "pink", "palegreen"))
 
-ggplot(clean_data_random, aes(x = dmy(date_published))) +
-  geom_bar() +
+ggplot(clean_data_random, aes(x = dmy(date_published), colour = journal)) +
+  geom_density() +
   ylab("Number of papers")
+geom_line()
